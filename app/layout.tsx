605c3976619d395dc5813182618497f24d32bf74
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 16:26:25 -0700

    adding first clerk configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Noto_Sans_Display } from "next/font/google"
+import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css"
 
 const noto = Noto_Sans_Display({ subsets: ["latin"] })
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={noto.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={noto.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
