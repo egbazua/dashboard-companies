35a2e8413f793e0cb027e5696eacd8c296606d11
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 16:41:26 -0700

    adding layout file for auth

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LayoutAuth = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='h-full flex flex-col justify-center items-center'>
+      <p>Dashboard Companies</p>
+      <h1 className='text-3xl my-2'>Welcome to my Dashboard</h1>
+      <h2 className='text-2xl mb-3'>Dashboard Companies</h2>
+      {children}
+    </div>
+  )
+}
+
+export default LayoutAuth
