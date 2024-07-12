e187704061dfd0816fa169e01b71a99b86b707a1
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 22:47:17 -0700

    creating graphic subscribers component

---
+++ app/(routes)/components/GraphicSubscribers/GraphicSubscribers.data.ts
@@ -0,0 +1,47 @@
+export const dataGraphics = [
+  {
+    year: "2016",
+    newCustomers: 4000,
+    oldCustomers: 2400,
+  },
+  {
+    year: "2017",
+    newCustomers: 4200,
+    oldCustomers: 2430,
+  },
+  {
+    year: "2018",
+    newCustomers: 1000,
+    oldCustomers: 2300,
+  },
+  {
+    year: "2019",
+    newCustomers: 4530,
+    oldCustomers: 1110,
+  },
+  {
+    year: "2020",
+    newCustomers: 2000,
+    oldCustomers: 3400,
+  },
+  {
+    year: "2021",
+    newCustomers: 6000,
+    oldCustomers: 3400,
+  },
+  {
+    year: "2022",
+    newCustomers: 5100,
+    oldCustomers: 8321,
+  },
+  {
+    year: "2023",
+    newCustomers: 3000,
+    oldCustomers: 1200,
+  },
+  {
+    year: "2024",
+    newCustomers: 2200,
+    oldCustomers: 3898,
+  },
+]
