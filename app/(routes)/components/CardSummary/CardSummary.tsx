545d561142d5ee68eee21e06df4878df68c8294c
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 17:09:22 -0700

    implementing CustomTooltip in card summary component

--- app/(routes)/components/CardSummary/CardSummary.tsx
+++ app/(routes)/components/CardSummary/CardSummary.tsx
@@ -1,12 +1,16 @@
 import CustomIcon from "@/components/CustomIcon/CustomIcon"
 import { CardSummaryProps } from "./CardSummary.types"
+import CustomTooltip from "@/components/CustomTooltip/CustomTooltip"
 
 const CardSummary = ({ average, icon: Icon, title, tooltipText, total }: CardSummaryProps) => {
   return (
     <div className="shadow-sm bg-background rounded-lg p-5 py-3 hover:shadow-lg transition">
       <div className="flex justify-between">
-        <CustomIcon icon={Icon} />
-        {title}
+        <div className="flex gap-2 items-center">
+          <CustomIcon icon={Icon} />
+          {title}
+        </div>
+        <CustomTooltip content={tooltipText} />
       </div>
     </div>
   )
