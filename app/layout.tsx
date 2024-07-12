1d7bc2a3e78467cd8f7c0437be9a9dbb8545ae1f
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 20:03:39 -0700

    adding theme provider configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Noto_Sans_Display } from "next/font/google"
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/ThemeProvider"
 import "./globals.css"
 
 const noto = Noto_Sans_Display({ subsets: ["latin"] })
@@ -18,7 +19,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={noto.className}>{children}</body>
+        <body className={noto.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
