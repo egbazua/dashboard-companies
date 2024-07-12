3fb679fd15dac4869b05a5ef128d0fa94bf4cf39
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-11 19:49:29 -0700

    adding more sidebar items in sidebar routes

--- components/SidebarItem/SidebarItem.tsx
+++ components/SidebarItem/SidebarItem.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import { SidebarItemProps } from "./SidebarItem.types"
 import { cn } from "@/lib/utils"
 
 const SidebarItem = ({ item: { href, icon: Icon, label } }: SidebarItemProps) => {
+  const pathname = usePathname()
+
   return (
-    <Link href={href} className={cn(`flex gap-x-2 mt-2 light:text-slate-700 dark:text-white text-sm items-center hover:bg-slate-300/20 p-2 rounded-lg cursor-pointer`)}>
+    <Link href={href} className={cn(`flex gap-x-2 mt-2 light:text-slate-700 dark:text-white text-sm items-center hover:bg-slate-300/20 p-2 rounded-lg cursor-pointer`, pathname === href && 'bg-slate-400/20')}>
       <Icon className="h-5 w-5" strokeWidth={1} />
       {label}
     </Link>
--- components/SidebarRoutes/SidebarRoutes.tsx
+++ components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import SidebarItem from "../SidebarItem/SidebarItem"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 import { dataGeneralSidebar, dataSupportSidebar, dataToolsSidebar } from "./SidebarRoutes.data"
 
 const SidebarRoutes = () => {
@@ -6,13 +10,42 @@ const SidebarRoutes = () => {
     <div className="flex flex-col justify-between h-full">
       <div>
         <div className="p-2 md:p-6">
-          <p>GENERAL</p>
+          <p className="text-slate-500 mb-2">GENERAL</p>
           {
             dataGeneralSidebar.map((item) => (
               <SidebarItem key={item.label} item={item} />
             ))
           }
         </div>
+        <Separator />
+        <div className="p-2 md:p-6">
+          <p className="text-slate-500 mb-2">TOOLS</p>
+          {
+            dataToolsSidebar.map((item) => (
+              <SidebarItem key={item.label} item={item} />
+            ))
+          }
+        </div>
+        <Separator />
+        <div className="p-2 md:p-6">
+          <p className="text-slate-500 mb-2">SUPPORT</p>
+          {
+            dataSupportSidebar.map((item) => (
+              <SidebarItem key={item.label} item={item} />
+            ))
+          }
+        </div>
+      </div>
+      <div>
+          <div className="text-center p-6">
+            <Button variant="outline" className='w-full'>
+              Upgrade Plan
+            </Button>
+          </div>
+          <Separator />
+          <footer className="mt-3 p-3 text-center">
+            2024. All rights reserved.
+          </footer>
       </div>
     </div>
   )
