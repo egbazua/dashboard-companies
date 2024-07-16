363cae852372a01bb3f4919e4136ff56e3ac69ca
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-16 02:15:35 -0700

    creating event api route

---
+++ app/api/company/[companyId]/event/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request, { params }: { params: { companyId: string } }) {
+  try {
+    const { userId } = auth()
+
+    const data = await req.json()
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+
+    const company = await db.company.findUnique({
+      where: {
+        id: params.companyId,
+      }
+    })
+
+    if (!company) return new NextResponse("Company not found", { status: 404 })
+    
+    const event = await db.event.create({
+      data: {
+        companyId: params.companyId,
+        ...data,
+      }
+    })
+
+    return NextResponse.json(event)
+  } catch (error) {
+    console.error(error)
+
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
