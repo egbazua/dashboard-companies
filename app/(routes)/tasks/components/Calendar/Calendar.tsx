9c4dfc8a37641f6d93f4232afd0314d326d4bc62
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-16 01:00:01 -0700

    Calendar component importations

--- app/(routes)/tasks/components/Calendar/Calendar.tsx
+++ app/(routes)/tasks/components/Calendar/Calendar.tsx
@@ -1,4 +1,18 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import FullCalendar from '@fullcalendar/react'
+import multiMonth from '@fullcalendar/multimonth'
+import dayGridPlugon from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+import { DateSelectArg, EventContentArg } from '@fullcalendar/core/index.js'
 import { CalendarProps } from "./Calendar.types"
+import { formatDate } from '@/lib/formatDate'
+import { Toast } from '@/components/ui/toast'
 
 const Calendar = ({ companies, events }: CalendarProps) => {
   return (
