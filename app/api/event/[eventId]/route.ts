60f3b0c775c3bfdd3a874cd99a43b1ad56e3b5e3
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-16 02:25:34 -0700

    adding delete event route

---
+++ app/api/event/[eventId]/route.ts
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+
+export async function DELETE(req: Request, { params }: { params: { eventId: string } }) {
+  try {
+    const { userId } = auth()
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+
+    const deletedEvent = await db.event.delete({
+      where: {
+        id: params.eventId,
+      },
+    })
+
+    return NextResponse.json(deletedEvent)
+  } catch (error) {
+    console.error(error)
+
+    return new NextResponse("Internal Error", { status: 500 })
+  }
+}
